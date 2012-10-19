3c4512890723319913e57bb36ebe27230e42ab05
AbhishekBiswal <abhishekbiswal@live.com>
2012-10-19 17:58:24 +0530

    1210 backup

--- pub/dash.php
+++ pub/dash.php
@@ -8,11 +8,41 @@
 	}
 	$pageName = "Quizr Dash";
 	include('temp/header.php');
+	include('db.php');
+
+	/*
+	
+	TODO:
+	Unpublished quizzes and published ones. send user to /q/123 and display a button to publish the quiz, only if the guy viewing it is the creator.
+
+	*/
 ?>
 
 <div class="det-head">
 	<h2>Dashboard</h2>
 </div>
+
+<div class="u-page-box ten columns">
+
+	<div class="u-page-header">Unpublished Quizzes:</div>
+	<ul class="u-page-list">
+		<!-- <li><span class="grey">20 Questions</span> <a href="/">First Quiz :P</a></li> -->
+	<?php
+
+	/* To load the unpublished quizzes */
+	$checkUnpub = $DBH->prepare("SELECT * FROM quizmeta WHERE user=? AND pub=0");
+	$checkUnpub->execute(array($curUser));
+	while($row = $checkUnpub->fetch())
+	{
+		
+	}
+
+	?>
+	</ul>
+
+</div>
+
+
 
 <?php
 	include('temp/footer.php');
