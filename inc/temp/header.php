d18fe9929e1d8f55a45ec87cce8322d707a728eb
Abhishek Biswal <abhishekbiswal@live.com>
2013-01-09 12:41:16 +0530

    fixed gitignore and added new header

--- inc/temp/header.php
+++ inc/temp/header.php
@@ -12,12 +12,10 @@
 
 	<link href="/assets/css/grid.css" rel="stylesheet" type="text/css">
 	<link href="/assets/css/style.css" rel="stylesheet" type="text/css">
-	<link href="/assets/css/edit.css" rel="stylesheet" type="text/css">
 	<link href="/assets/css/ab.css" rel="stylesheet" type="text/css">
-	<link href="/assets/css/sb.css" rel="stylesheet" type="text/css">
 	<script src="/assets/js/jquery.js"></script>
 	<script src="/assets/js/main.js"></script>
-	<script src="/assets/js/moment.js"></script>
+	<?php if(@$page == "questionslist") echo '<script src="/assets/js/jqueryui.js"></script>'; ?>
 </head>
 <body>
 
